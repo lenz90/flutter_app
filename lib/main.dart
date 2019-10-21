836b1ef0e488d0f3af3ece853111a69fea81c38a
lenz90 <lenz.90@gmail.com>
2019-10-20 22:33:19 -0500

    Se agrega la  navigation tabBarView

--- lib/main.dart
+++ lib/main.dart
@@ -65,54 +65,35 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.call),
-            tooltip: "Call contact",
-            onPressed: callContact,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.directions_railway),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_subway),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_bike),
+                ),
+              ],
+            ),
+            title: Text("Tab Bar")
           ),
-          IconButton(
-            icon: Icon(Icons.add),
-            tooltip: "Add Information to contact",
-            onPressed: addInfoContact,
-          )
-        ],
-      ),
-      body: Center(
-
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Principal")
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_railway),
+              Icon(Icons.directions_subway),
+              Icon(Icons.directions_bike),
+            ],
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              title: Text("Negocios")
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              title: Text("School")
-          ),
-
-        ],
-        currentIndex: _selectedIndex,
-        onTap: itemTapped,
-        fixedColor: Colors.black,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.airplay),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+      )
     );
   }
 
