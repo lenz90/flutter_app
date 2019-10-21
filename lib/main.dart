e90ccb3067924e5a6b028303772a857e14c9ac2d
lenz90 <lenz.90@gmail.com>
2019-10-20 22:42:26 -0500

    Se agrega la  navigation Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -65,35 +65,70 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.directions_railway),
-                ),
-                Tab(
-                  icon: Icon(Icons.directions_subway),
-                ),
-                Tab(
-                  icon: Icon(Icons.directions_bike),
-                ),
-              ],
-            ),
-            title: Text("Tab Bar")
+    return Scaffold(
+      appBar: AppBar(
+
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.call),
+            tooltip: "Call contact",
+            onPressed: callContact,
           ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.directions_railway),
-              Icon(Icons.directions_subway),
-              Icon(Icons.directions_bike),
-            ],
-          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            tooltip: "Add Information to contact",
+            onPressed: addInfoContact,
+          )
+        ],
+      ),
+      
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text("Alarm"),
+              onTap: () {
+                // Cambia el estado de la aplicacion
+                print("Cambia la pagina");
+                Navigator.pop(context);
+              },
+            )
+          ],
         ),
-      )
+      ),
+      body: Center(
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Principal")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              title: Text("Negocios")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              title: Text("School")
+          ),
+
+        ],
+        currentIndex: _selectedIndex,
+        onTap: itemTapped,
+        fixedColor: Colors.black,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.airplay),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
