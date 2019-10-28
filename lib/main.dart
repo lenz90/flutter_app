a6cb78d923c859aa3fa9bd93af53000a9f7192ea
lenz90 <lenz.90@gmail.com>
2019-10-27 21:06:46 -0500

    Se agrega el compormente CupertinoPageScafod

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<ScaffoldState> _scaffold = new GlobalKey<ScaffoldState>();
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -55,26 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      key: _scaffold,
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
-          child: CupertinoButton(
-            child: Text("Click me"),
-            onPressed: () {
-              print("You have clicked me");
-            },
-            color: Colors.red,
-            borderRadius: BorderRadius.all(Radius.circular(40)),
-          )
-      ),
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          trailing: CupertinoButton(
+              child: Text("Save")
+          ),
+          backgroundColor: Colors.red,
+        ),
+        child: SafeArea(
+            child: Container(
+              child: Text("Hola Mundole"),
+            )
+        )
     );
   }
 }
