455b28a11d2c9c58fe97d1bab7e34b96053ac5a0
lenz90 <lenz.90@gmail.com>
2019-11-14 23:23:36 -0500

    Se agrega la funcionalidad que llama a código java y le entrega variables

--- android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.flutter_app;
 
 import android.os.Bundle;
+
+import java.util.Map;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "com.flutter.epic/epic";
@@ -18,8 +22,15 @@ public class MainActivity extends FlutterActivity {
                 .setMethodCallHandler(new MethodChannel.MethodCallHandler() {
                   @Override
                   public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                      final Map<String, Object> args = methodCall.arguments();
                       if (methodCall.method.equals("Printy")) {
-                          result.success("Hi From Java");
+                          String val1 = (String) args.get("val1");
+                          if(val1.equals("1")) {
+                              result.success("Yay");
+                          } else {
+                              result.success("NAy");
+                          }
+
                       }
                   }
               }
